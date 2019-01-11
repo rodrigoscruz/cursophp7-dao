cf807e2095b33b9db5283a650c161e42fc54fedc
Rodrigo souza <rs.strat@gmail.com>
2019-01-10 23:56:16 -0200

    inicio projeto

---
+++ class/Sql.php
@@ -0,0 +1,46 @@
+<?php
+
+class Sql extends PDO {
+    
+    private $conn;
+    
+    public function _construct(){
+        
+        $this->conn = new PDO("mysql:host=localhost;dbname=cadastro","root","");
+    }
+    
+    private function setParams($statment,$parameters = array()){
+        
+        foreach ($parameters as $key => $value){
+            
+            $this->setParam($key,$value);
+        }
+    }
+    
+    private function setParam($statment,$key,$value){
+        
+        $statment->bindParam($key,$value);
+    }
+    
+    public function query($rawQuery,$params = array()){
+        
+        $stmt = $this->conn->prepare($rawQuery);
+        
+        $this->setParams($stmt,$params);
+        
+        $stmt->execute();
+        
+        return $stmt;
+    }
+    
+    public function select($rawQuery,$params = array()):array
+    {
+        
+        $stmr = $this->query($rawQuery, $params);
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+
+?>
